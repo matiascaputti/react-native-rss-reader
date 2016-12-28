e7cb7bd1c1462765c1b6be9e4e2120f2db1d04d1
maticaputti <matias.caputti@gmail.com>
2016-12-27 22:23:42 -0300

    Add .gitignore

--- node_modules/@exponent/ex-navigation/src/drawer/ExNavigationDrawerItem.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default class ExNavigationDrawerItem extends React.Component {
-  render() {
-    return null;
-  }
-}
--- node_modules/@exponent/ex-navigation/src/sliding-tab/ExNavigationSlidingTabItem.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default class ExNavigationSlidingTabItem extends React.Component {
-  render() {
-    return null;
-  }
-}
